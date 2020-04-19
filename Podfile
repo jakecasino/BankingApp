ed6bd76e91c566d96d11264a2f6c7843ed845b74
Jake Bryan Casino <jake@jakecasino.com>
2020-04-18 21:00:41 -0700

    Added Firebase

--- Podfile
+++ Podfile
@@ -1,13 +1,15 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '13.0'
+  platform :ios, '13.4.1'
 
-target 'Banking App', 'Banking App Sandbox' do
+target 'Banking App' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Banking App
   pod 'Alamofire'
   pod 'Plaid'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
 
   target 'Banking AppTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Banking App/Pods-Banking App-frameworks.sh
+++ Pods/Target Support Files/Pods-Banking App/Pods-Banking App-frameworks.sh
@@ -198,11 +198,27 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${PODS_ROOT}/Plaid/LinkKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${PODS_ROOT}/Plaid/LinkKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
+fi
+if [[ "$CONFIGURATION" == "Beta" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${PODS_ROOT}/Plaid/LinkKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
